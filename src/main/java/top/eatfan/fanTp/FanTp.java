b10b3e86f71e9556662f405b8cdc33fad1ef16d5
EatFan <eatfan0921@163.com>
2025-05-16 15:15:19 +0800

    更新：注册指令

--- src/main/java/top/eatfan/fanTp/FanTp.java
+++ src/main/java/top/eatfan/fanTp/FanTp.java
@@ -3,10 +3,13 @@ package top.eatfan.fanTp;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class FanTp extends JavaPlugin {
+    private static FanTp instance;
 
     @Override
     public void onEnable() {
         getLogger().info("插件正在启动中...");
+        instance = this;
+
 
         getLogger().info("插件启动完毕");
     }
@@ -17,5 +20,9 @@ public final class FanTp extends JavaPlugin {
 
         getLogger().info("插件关闭成功！");
 
+    }
+
+    public static FanTp getInstance() {
+        return instance;
     }
 }
