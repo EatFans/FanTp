c215c0cb511f6e153d5f02ba26fbf09742457f30
EatFan <eatfan0921@163.com>
2025-05-16 15:09:01 +0800

    初始化项目

---
+++ src/main/java/top/eatfan/fanTp/FanTp.java
@@ -0,0 +1,21 @@
+package top.eatfan.fanTp;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class FanTp extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        getLogger().info("插件正在启动中...");
+
+        getLogger().info("插件启动完毕");
+    }
+
+    @Override
+    public void onDisable() {
+        getLogger().info("插件正在关闭中...");
+
+        getLogger().info("插件关闭成功！");
+
+    }
+}
