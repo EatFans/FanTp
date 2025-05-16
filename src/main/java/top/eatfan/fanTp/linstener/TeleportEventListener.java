86c448764994809e6e22f8cfcdcfdf2e03ebc0c9
EatFan <eatfan0921@163.com>
2025-05-16 21:49:43 +0800

    更新：传送请求事件处理

--- src/main/java/top/eatfan/fanTp/linstener/TeleportEventListener.java
+++ src/main/java/top/eatfan/fanTp/linstener/TeleportEventListener.java
@@ -1,10 +1,18 @@
 package top.eatfan.fanTp.linstener;
 
+import net.md_5.bungee.api.chat.ClickEvent;
+import net.md_5.bungee.api.chat.ComponentBuilder;
+import net.md_5.bungee.api.chat.HoverEvent;
+import net.md_5.bungee.api.chat.TextComponent;
+import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import top.eatfan.fanTp.FanTp;
 import top.eatfan.fanTp.event.TeleportRequestSendEvent;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * 传送事件监听器
@@ -28,7 +36,43 @@ public class TeleportEventListener implements Listener {
         Player sender = event.getSender();
         Player targetPlayer = event.getTargetPlayer();
 
-        sender.sendMessage("你正在给"+targetPlayer.getName()+"发送传送请求");
-        targetPlayer.sendMessage(sender.getName() + "请求传送到你这里");
+        // 创建传送请求
+        plugin.getTeleportRequestManager().addRequest(sender,targetPlayer);
+
+        sender.sendMessage(ChatColor.translateAlternateColorCodes('&',"&a已经向 &f&l" + targetPlayer.getName() + " &a发送了传送请求！"));
+
+        targetPlayer.sendMessage(" ");
+        targetPlayer.sendMessage(ChatColor.translateAlternateColorCodes('&',"&a玩家 &f&l" + sender.getName() + " &a请求传送到你的位置，是否接受？"));
+        targetPlayer.sendMessage(" ");
+
+        TextComponent space = new TextComponent("  "); // 两个空格
+        TextComponent agree = new TextComponent("[ 同意 ]");
+        agree.setColor(net.md_5.bungee.api.ChatColor.GREEN);
+        agree.setBold(true);
+        agree.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND,"/tpa yes"));
+        agree.setHoverEvent(new HoverEvent(
+                HoverEvent.Action.SHOW_TEXT,
+                new ComponentBuilder("点击以接受传送请求").color(net.md_5.bungee.api.ChatColor.YELLOW).create()
+        ));
+        TextComponent deny = new TextComponent("[ 拒绝 ]");
+        deny.setColor(net.md_5.bungee.api.ChatColor.RED);
+        deny.setBold(true);
+        deny.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND,"/tpa no"));
+        deny.setHoverEvent(new HoverEvent(
+                HoverEvent.Action.SHOW_TEXT,
+                new ComponentBuilder("点击以拒绝传送请求").color(net.md_5.bungee.api.ChatColor.YELLOW).create()
+                ));
+
+        TextComponent fullMessage = new TextComponent();
+        fullMessage.addExtra(space);
+        fullMessage.addExtra(space);
+        fullMessage.addExtra(agree);
+        fullMessage.addExtra(space);
+        fullMessage.addExtra(space);
+        fullMessage.addExtra(deny);
+
+        targetPlayer.spigot().sendMessage(fullMessage);
+        targetPlayer.sendMessage(" ");
     }
+
 }
